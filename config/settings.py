8111057a0247b55bcb8a7c4e522459c0df39d7fb
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-24 17:18:34 +0200

    Update INSTALLED_APPS with theatre, drf and jwt

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'drf_spectacular',
+    'django_filters',
+    'rest_framework_simplejwt.token_blacklist',
+
+    'theatre',
 ]
 
 MIDDLEWARE = [
