76ccd9c54abae11ec335a4533b71e284a4deff32
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-24 17:40:59 +0200

    Register models Actor, Genre, Play, Ticket, Reservation, Perfomance and TheatreHall in Admin panel

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Genre, Actor, Play, TheatreHall, Performance, Reservation, Ticket
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name")
+    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+    filter_horizontal = ("actors", "genres")
+
+
+@admin.register(TheatreHall)
+class TheatreHallAdmin(admin.ModelAdmin):
+    list_display = ("name", "rows", "seats_in_row", "capacity")
+    search_fields = ("name",)
+
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ("play", "theatre_hall", "show_time")
+    list_filter = ("theatre_hall", "show_time")
+    search_fields = ("play__title", "theatre_hall__name")
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 0
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("user__username",)
+    inlines = [TicketInline]
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("id", "performance", "row", "seat", "reservation")
+    list_filter = ("performance",)
+    search_fields = ("performance__play__title",)
